ae5d7d297a23ef8b2440b3d27e8766fbc74dc5f7
jhung-mililani <115854825+jhung-mililani@users.noreply.github.com>
2025-02-21 15:51:06 -0700

    update project references and links for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,29 +17,27 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            CS-212 <span className="text-[hsl(280,100%,70%)]">A3-Storefront</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
+              href="https://github.com/jhung-mililani/a3-storefront"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
+              <h3 className="text-2xl font-bold">GitHub Repository →</h3>
               <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+                Our private repo for the CS-212 A3-Storefront project. Make sure you're logged in!
               </div>
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
+              href="https://www.notion.so/A3-Storefront-1a1feea808e881b6afa5d433a3f8e5fb"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">Documentation →</h3>
               <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
+                Our Notion teamspace is where our project information is organized. Make sure to check it regularly for updates and new information!
               </div>
             </Link>
           </div>
