7b0bfa65e707d61dd87b2784a89a73a1855947fd
jhung-mililani <115854825+jhung-mililani@users.noreply.github.com>
2025-03-04 22:03:26 -0700

    feat: integrate Apercu font and update layout to use it in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 import * as tailwindcssAnimate from "tailwindcss-animate";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   darkMode: ["class"],
@@ -8,7 +8,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        sans: ["var(--font-apercu)", ...fontFamily.sans],
       },
       borderRadius: {
         lg: "var(--radius)",
